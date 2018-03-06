9af5c07358123c6850eb411811b20986c9a28442
Vashishti <szg0081@auburn.edu>
2018-03-06 12:34:29 -0600

    create actions and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,7 @@
-import { combineReducers } from 'redux'
-
 import {
   ADD_RECIPE,
   REMOVE_FROM_CALENDAR,
 } from '../actions'
-
-function food (state = {}, action) {
-  switch (action.type) {
-    case ADD_RECIPE :
-      const { recipe } = action
-
-      return {
-        ...state,
-        [recipe.label]: recipe,
-      }
-    default :
-      return state
-  }
-}
 
 const initialCalendarState = {
   sunday: {
@@ -82,7 +66,4 @@ function calendar (state = initialCalendarState, action) {
   }
 }
 
-export default combineReducers({
-  food,
-  calendar,
-})
+export default calendar
